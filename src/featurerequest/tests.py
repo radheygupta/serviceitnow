6ba948b02bceeba2d7e3a925cab57b48e8c70078
Radhey <radhey0105@gmail.com>
2018-06-22 01:39:23 +0530

    added unit test

--- src/featurerequest/tests.py
+++ src/featurerequest/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Features, Clients, ProductArea
+from datetime import datetime
 
-# Create your tests here.
+
+class FeatureTestCase(TestCase):
+    def setUp(self):
+        c1 = Clients.objects.create(name="ClientA", address="Noida", enddate="2019-12-02", status="A")
+        p1 = ProductArea.objects.create(name="Billing", description="Billing", status="A")
+        f1 = Features.objects.create(title="Test1", description="This is description", client=c1, priority=1,
+                                     target_date="2018-12-02", product_area=p1, status="A")
+        f2 = Features.objects.create(title="Test2", description="This is description", client=c1, priority=2,
+                                     target_date="2018-12-02", product_area=p1, status="A")
+
+    def test_feature_start_date(self):
+        f1 = Features.objects.get(title="Test1")
+        self.assertEqual(f1.start_date.strftime('%Y-%m-%d'), datetime.now().strftime('%Y-%m-%d'))
